205a39c783ba364793c6fe677c67244e3e1ad69b
Maki <mxmcube@gmail.com>
2020-06-24 21:58:46 +0100

    Updated metaverse to tivolicloud.com

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export const displayPlural = (n: number, singular: string, plural?: string) =>
 	n + " " + (n == 1 ? singular : plural != null ? plural : singular + "s");
 
-export const METAVERSE_URL = "https://alpha.tivolicloud.com";
+export const METAVERSE_URL = "https://tivolicloud.com";
